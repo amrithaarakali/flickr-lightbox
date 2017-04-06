3c974a33351461c5e231cf1e7e90290b02e9cfd4
Amritha <aarakali@paypal.com>
2017-04-05 17:55:08 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# flickr-lightbox
-Gallery to display images and view them in a lightbox
+Photo Lightbox
+===========
+
+Go to - <link> for a live demo.
+
+- The app pulls images from a public Flickr album using the Flickr API.
+- View the photos from the album on the gallery.
+- Clicking on a photo opens up the lightbox.
+- Navigate through the album by clicking on the prev or next buttons.
+
+
---
+++ lightbox.js
@@ -0,0 +1,149 @@
+'use strict';
+
+// global variables used to maintain state between functions
+let currentImage;
+let photoSet;
+
+// init function to get photos from the API, set up the gallery with thumbnails and sets up the toggle buttons.
+
+function init() {
+    // photos from the album
+    photoSet = getPhotos();
+    // populate gallery with thumbnails of the images from the album
+    populateGallery(photoSet);
+    // add click handlers to previous and next buttons
+    setUpToggleButtons();
+}
+
+function getPhotos() {
+    // flickr key and photoset id
+    const flickrKey = '64be1d8cf57f604b23b74346b7e325c1';
+    const photosetId = '72157677269263392';
+
+    // setup Flickr API for photoset
+    const url = 'https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=' + flickrKey + '&photoset_id=' + photosetId + '&extras=url_s,url_m&format=json&nojsoncallback=1';
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url, false);
+    xhttp.send();
+
+    // parse response as JSON if return status is success
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+        let response = JSON.parse(xhttp.responseText);
+        let albumImages = response.photoset;
+        return albumImages && albumImages.photo;
+    }
+    // TODO handle API errors
+}
+
+function populateGallery(photoSet) {
+
+    // build DOM structures with images
+    for (let i = 0; i < photoSet.length; i++) {
+        let galleryItem = document.createElement('div');
+        let galleryImage = document.createElement('img');
+        galleryImage.id = photoSet[i].id;
+        galleryImage.src = photoSet[i].url_m;
+        galleryImage.setAttribute('data-title', photoSet[i].title);
+
+        // add click handler for each image
+        galleryImage.addEventListener('click', selectImage, false);
+
+        // add each image to a parent container
+        galleryItem.appendChild(galleryImage);
+        galleryItem.className = 'cell';
+
+        // add all image containers to the main gallery container
+        document.getElementById('galleryContainer').appendChild(galleryItem);
+    }
+}
+
+function setUpToggleButtons() {
+    let prevButton = document.getElementById('previousBtn');
+    let nextButton = document.getElementById('nextBtn');
+    prevButton.addEventListener('click', handlePrev);
+    nextButton.addEventListener('click', handleNext);
+    document.getElementById('close').addEventListener('click', handleClose);
+
+    // add event listeners for keyboard shortcuts
+    window.addEventListener('keydown', handlePrev);
+    window.addEventListener('keydown', handleNext);
+    window.addEventListener('keydown', handleClose);
+}
+
+// setup the lightbox and handle selection, toggling of images.
+function setUpLightbox(id) {
+
+    // clear the 'current' context of the image displayed before change
+    if (document.getElementsByClassName('current').length > 0) {
+        let prev = document.getElementsByClassName('current')[0];
+        prev.className = prev.className.replace( /current/g , '' )
+    }
+
+    // add 'current' context to the new image after change
+    currentImage = document.getElementById(id);
+    currentImage.className += ' current';
+
+    document.getElementById('lightboxContainer').style.display = 'block';
+
+    // change the image displayed in the lightbox by replacing the image source and the title
+    document.getElementById('lightboxImg').src = currentImage.src;
+    document.getElementById('lightboxTitle').innerHTML = currentImage.getAttribute('data-title');
+}
+
+// select image based on the gallery thumbnail clicked
+function selectImage(event) {
+    setUpLightbox(event.target.id);
+    event.stopPropagation();
+}
+
+// handling the next button click
+function handleNext (event) {
+
+    // find index of current image in the list of photos in the album
+    let index = photoSet.findIndex(x => x.id === currentImage.id);
+    let targetId = event.target.id;
+
+    // verify if there's a target id from the button click or the keyboard shortcut is pressed
+    if (targetId || event.which === 39) {
+
+        if (index < photoSet.length - 1) {
+            //update the current image to the next image in the album
+            currentImage = photoSet[index + 1];
+        }
+        else {
+            // If current image is the last image in the album, current image is not updated.
+            // Lets the user know they've reached the end of the album
+            currentImage = photoSet[index];
+        }
+
+        // update ligthbox after change
+        setUpLightbox(currentImage.id);
+    }
+}
+
+function handlePrev (event) {
+    // TODO move to a common function
+    let index = photoSet.findIndex(x => x.id === currentImage.id);
+    let targetId = event.target.id;
+
+    if(targetId || event.keyCode === 37) {
+        //update the current image to the prev image in the album
+        if(index > 0) {
+            currentImage = photoSet[index - 1];
+        }
+        else {
+            // If current image is the first image in the album, current image is not updated.
+            currentImage = photoSet[0];
+        }
+        setUpLightbox(currentImage.id);
+    }
+}
+
+function handleClose (event) {
+    if( event.target.id || event.keyCode === 27) {
+        // hide the lightbox
+        document.getElementById('lightboxContainer').style.display = 'none';
+    }
+}
+
+window.onload = init();
